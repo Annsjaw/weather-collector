f3ab14622271b449dc0abac7b122950da8833b8c
Dmitry Rotanin <rotanin@gmail.com>
2023-06-03 06:30:53 +0600

    del commented string

--- src/database.py
+++ src/database.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from config import settings
 
-# from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
 DB_URL = f'postgresql://{settings.DB_USER}:{settings.DB_PASS}@' \
          f'{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 
